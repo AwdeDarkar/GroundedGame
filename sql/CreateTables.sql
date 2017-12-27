974e8c552355dbf91164362a53fd3a20b376a670
Nathan Martindale <nmblenderdude0@gmail.com>
2017-12-26 23:24:51 -0500

    Created table creation sql script

---
+++ sql/CreateTables.sql
@@ -0,0 +1,154 @@
+# copyright 2017 AWDE
+# date created: 12/26/2017
+# date edited: 12/26/2017
+
+create table Users (
+	ID int unsigned primary key auto_increment,
+	Name char(30),
+	Hash char(128),
+	Verification char(128),
+	Email char(200),
+	DateJoined date,
+	Level tinyint,
+	NameSafe char(30)
+);
+
+create table Worlds (
+	ID int unsigned primary key auto_increment,
+	Name char(30)
+);
+
+create table Factions (
+	ID int unsigned primary key auto_increment,
+	UserID int unsigned,
+	WorldID int unsigned,
+	Name char(30)
+);
+
+create table Bunkers (
+	ID int unsigned primary key auto_increment,
+	WorldID int unsigned,
+	FactionID int unsigned,
+	WorldX int,
+	WorldY int
+);
+
+create table Entities (
+	ID int unsigned primary key auto_increment,
+	FactionID int unsigned,
+);
+
+create table Resources (
+	ID int unsigned primary key auto_increment,
+	Name char(30),
+	Description text
+);
+
+create table ResourceCollections (
+	ID int unsigned primary key auto_increment,
+	EntityID int unsigned,
+	BunkerID int unsigned,
+	FactionID int unsigned,
+	ResourceID int unsigned,
+);
+
+create table Soldiers (
+	RCID int unsigned,
+	Hitpoints int unsigned,
+	Type int unsigned,
+
+	pimary key(RCID, Hitpoints, Type)
+);
+
+create table Traps (
+	RCID int unsigned,
+	pimary key(RCID)
+);
+
+create table Rooms (
+	EntityID int unsigned primary key,
+	BunkerID int unsigned,
+	Name char(30),
+	GridX int,
+	GridY int,
+	Width int,
+	Height int,
+	ConnUp bool,
+	ConnDown bool,
+	ConnRight bool,
+	ConnLeft bool,
+);
+
+create table SurfaceUnits (
+	EntityID int unsigned primary key,
+	FactionID int unsigned,
+	WorldX int,
+	WorldY int
+);
+
+create table News (
+	ID int unsigned primary key auto_increment,
+	FactionID int unsigned,
+	Title char(30),
+	Content text,
+	PostDate date
+);
+
+create table ProductionJobs (
+	ID int unsigned primary key auto_increment,
+	FactionID int unsigned,
+	RCID1 int unsigned,
+	RCID2 int unsigned,
+	ResultResourceID int unsigned,
+	ResultAmount int unsigned,
+	StartDate datetime,
+	EndDate datetime
+);
+
+create table MovementJobs (
+	ID int unsigned primary key auto_increment,
+	EntityID int unsigned,
+	SourceBunkerID int unsigned,
+	DestinationBunkerID int unsigned,
+	FactionID int unsigned,
+	StartDate datetime,
+	EndDate datetime
+);
+
+create table Orders (
+	ID int unsigned primary key auto_increment,
+	RCID int unsigned,
+	AmountRemaining int unsigned,
+	Cost int unsigned,
+	Status tinyint,
+	DatePosted datetime
+);
+
+create table Transactions (
+	ID int unsigned primary key auto_increment,
+	RCID int unsigned,
+	Amount int unsigned,
+	Cost int unsigned,
+	Status tinyint,
+	SellingFactionID int unsigned,
+	BuyingFactionID int unsigned
+);
+
+create table MessageGroups (
+	ID int unsigned primary key auto_increment,
+	WorldID int unsigned
+);
+
+create table MessageGroupParticipants (
+	MGID int unsigned,
+	FactionID int unsigned,
+	primary key(MGID, FactionID)
+);
+
+create table Messages (
+	SrcFactionID int unsigned,
+	MGID int unsigned,
+	DateSent datetime,
+	Content text,
+	primary key(SrcFactionID, MGID, DateSent)
+);
