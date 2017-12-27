3e665db57814dff452d07b94bd8ef23b71e39c15
Nathan Martindale <nmblenderdude0@gmail.com>
2017-12-27 09:17:44 -0500

    Added registration verification page

--- public_html/handle_user.php
+++ public_html/handle_user.php
@@ -1,6 +1,6 @@
 <?php
 //define(PERMISSION_LEVEL, -1);
-//include_once("../includes/common.php");
+include_once("../includes/common.php");
 
 include_once("../includes/register.php");
 include_once("../includes/login.php");
---
+++ public_html/verify.php
@@ -0,0 +1,36 @@
+<?php
+//define(PERMISSION_LEVEL, -1);
+include("../includes/common.php");
+
+if (isset($_GET['u'], $_GET['e'], $_GET['v']))
+{
+	$username = tools_sanitize_data($_GET['u']);
+	$email = tools_sanitize_data($_GET['e']);
+	$verification = tools_sanitize_data($_GET['v']);
+
+	if ($stmt = $mysqli->prepare("SELECT Level FROM Users WHERE NameSafe = ? AND Email = ? AND Verification = ?"))
+	{
+		$stmt->bind_param('sss', $username, $email, $verification);
+		$stmt->execute();
+		$stmt->store_result();
+
+		if ($stmt->num_rows() < 1) { throw_msg(406, "index.php"); }
+		
+		$stmt->bind_result($UserLevel);
+		$stmt->fetch();
+
+		if ($UserLevel != 0) { throw_msg(407, "index.php"); }
+	}
+	else { throw_msg(300, "index.php", "verify.php", 33); }
+
+	//update user permissions
+	if ($stmt = $mysqli->prepare("UPDATE Users Set Level = 1 WHERE afe = ? AND Email = ? AND Verification = ? LIMIT 1"))
+	{
+		$stmt->bind_param('sss', $username, $email, $verification);
+		$stmt->execute();
+		throw_msg(103, "index.php");
+	}
+	else { throw_msg(300, "index.php", "verify.php", 41); }
+}
+else { throw_msg(303, "index.php", "verify.php", 44); }
+?>
