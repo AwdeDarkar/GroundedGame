607ca19f408994179b4f7c77c9912fc8e2688757
Nathan Martindale <nmblenderdude0@gmail.com>
2017-12-28 11:11:08 -0600

    Fixed issue with sql load data

--- sql/ImportCsv.sql
+++ sql/ImportCsv.sql
@@ -2,19 +2,19 @@ TRUNCATE TABLE Resources;
 TRUNCATE TABLE Processes;
 TRUNCATE TABLE ProcessComponents;
 
-LOAD DATA INFILE 'resources.csv'
+LOAD DATA LOCAL INFILE 'resources.csv'
 	INTO TABLE Resources
 	FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"'
 	LINES TERMINATED BY '\n'
 	(ID,Name,NameSafe,Type,Frequency,Description);
 
-LOAD DATA INFILE 'process_components.csv'
+LOAD DATA LOCAL INFILE 'process_components.csv'
 	INTO TABLE ProcessComponents
 	FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"'
 	LINES TERMINATED BY '\n'
 	(PID,RID,Amount,Type);
 
-LOAD DATA INFILE 'processes.csv'
+LOAD DATA LOCAL INFILE 'processes.csv'
 	INTO TABLE Processes
 	FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"'
 	LINES TERMINATED BY '\n'
