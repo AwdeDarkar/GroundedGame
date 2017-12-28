2ff7394bf2975bede4947a870af898b2fbfd78da
Nathan Martindale <nmblenderdude0@gmail.com>
2017-12-28 15:35:36 -0600

    Added resource collection printing to bunker

--- public_html/bunker.php
+++ public_html/bunker.php
@@ -58,22 +58,22 @@ else { throw_msg(300, $httpReferer, "create_faction.php", 39); }
 
 
 # query all resource collections
-$rd_ids = array();
-$rd_rates = array();
-$rd_amts = array();
-$rd_maxes = array();
-if ($stmt = $mysqli->prepare("SELECT ResourceID, ReplenishRate, Amount, Maximum FROM ResourceDeposits WHERE BunkerID = ?"))
+$rc_ids = array();
+$rc_names = array();
+$rc_amts = array();
+$rc_types = array();
+if ($stmt = $mysqli->prepare("SELECT ResourceCollections.ID, Resources.Name, ResourceCollections.Amount, Resources.Type FROM Resources, ResourceCollections WHERE Resources.ID = ResourceCollections.ResourceID AND ResourceCollections.BunkerID = ?"))
 {
 	$stmt->bind_param('s', $bunkerID);
 	$stmt->execute();
 	$stmt->store_result();
-	$stmt->bind_result($resID, $rate, $amt, $max);
+	$stmt->bind_result($id, $name, $amt, $type);
 	while ($stmt->fetch())
 	{
-		array_push($rd_ids, $resID);
-		array_push($rd_rates, $rate);
-		array_push($rd_amts, $amt);
-		array_push($rd_maxes, $max);
+		array_push($rc_ids, $id);
+		array_push($rc_names, $name);
+		array_push($rc_amts, $amt);
+		array_push($rc_types, $type);
 	}
 }
 else { throw_msg(300, $httpReferer, "create_faction.php", 39); }
@@ -105,6 +105,26 @@ for ($i = 0; $i < count($rd_ids); $i++)
 			<td>".$rd_amts[$i]."</td>
 			<td>".$rd_maxes[$i]."</td>
 		</tr>");
+}
+?>
+</table>
+
+<h3>Resource Collections</h3>
+<table>
+	<tr>
+		<th>Resource</th>
+		<th>Amount</th>
+	</tr>
+<?php
+for ($i = 0; $i < count($rc_ids); $i++)
+{
+	echo("
+		<tr>
+			<td>".$rc_names[$i]."</td>
+			<td>".$rc_amts[$i]."</td>");
+	if ($rc_types[$i] == 2) { echo("<td>(Equipment)</td>"); }
+	else { echo("<td></td>"); }
+	echo("</tr>");
 }
 ?>
 </table>
