969cb93b6b85bc94e086b3c9bba3f42ce83145c5
Nathan Martindale <nmblenderdude0@gmail.com>
2017-12-27 20:33:10 -0500

    Fixed missing semicolon

--- public_html/ajax_faction_list.php
+++ public_html/ajax_faction_list.php
@@ -6,7 +6,7 @@ $world = $_GET['w'];
 
 if ($stmt = $mysqli->prepare("SELECT Factions.Name FROM Factions, Worlds WHERE Factions.WorldID = Worlds.ID AND Worlds.NameSafe = ?"))
 {
-	$stmt->bind_param('s', $world)
+	$stmt->bind_param('s', $world);
 	$stmt->execute();
 	$stmt->store_result();
 	$stmt->bind_result($Name);
