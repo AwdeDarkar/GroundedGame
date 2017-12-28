49a3c7a3555dd1e6044270d4debceb16d4abb59b
Nathan Martindale <nmblenderdude0@gmail.com>
2017-12-27 20:28:55 -0500

    Started faction list ajax point

---
+++ public_html/ajax_faction_list.php
@@ -0,0 +1,18 @@
+<?php
+define(PERMISSION_LEVEL, -1);
+include("../includes/common.php");
+
+$world = $_GET['w'];
+
+if ($stmt = $mysqli->prepare("SELECT Factions.Name FROM Factions, Worlds WHERE Factions.WorldID = Worlds.ID AND Worlds.NameSafe = ?"))
+{
+	$stmt->bind_param('s', $world)
+	$tempResult = $stmt->execute();
+	$stmt->store_result();
+	$stmt->bind_result($Name);
+	
+	while($stmt->fetch())
+	{
+		echo("<p>$Name</p>");
+	}
+}
