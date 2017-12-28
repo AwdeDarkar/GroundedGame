2fec9b66dfc87897bc6f7f05aec348d15dfc7ea8
Nathan Martindale <nmblenderdude0@gmail.com>
2017-12-27 20:06:48 -0500

    Actually started world map page

---
+++ includes/world.php
@@ -0,0 +1,31 @@
+<?php
+define(PERMISSION_LEVEL, 1);
+include("../includes/common.php");
+
+$world = $_GET['w'];
+$fac = -1;
+if (isset($_GET['f'])) { $fac = $_GET['f']; }
+
+$httpReferer = tools_get_referer("index.php");
+
+// get some pertinent info
+
+// get world name
+$worldname = "";
+if ($stmt = $mysqli->prepare("SELECT Name FROM Worlds WHERE NameSafe = ? LIMIT 1"))
+{
+	$stmt->bind_param('s', $worldname);
+	$stmt->execute();
+	$stmt->store_result();
+	$stmt->bind_result($worldname);
+	$stmt->fetch();
+
+	if ($UserCount > 0) { throw_msg(402, $httpReferer); }
+}
+else { throw_msg(300, $httpReferer, "create_faction.php", 39); }
+
+
+?>
+
+<h1><?php echo("$worldname"); ?> World Map </h1>
+
