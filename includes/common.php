57d518feff81d8a4b8181135140660a72bac63b4
AwdeDarkar <darkardengeno@gmail.com>
2018-01-20 22:32:52 -0500

    Pulled head code into header.php

--- includes/common.php
+++ includes/common.php
@@ -6,4 +6,5 @@ include_once("global_tools.php");
 include_once("message_handling.php");
 sec_session_start();
 include_once("check_permissions.php");
+require_once "../includes/external/crush/css-crush/CssCrush.php";
 ?>
---
+++ public_html/header.php
@@ -0,0 +1,27 @@
+<?php
+echo "<head>
+	<title>Grounded Game Dev</title>
+	<meta charset='UTF-8'>
+	<meta name='description' content='Development of the \'grounded\' game'>
+	<meta name='keywords' content='Incomplete,Testing'>
+	<meta name='author' content='WildfireXIII,Darkar'>
+	<meta name'viewport' content='width=device-width, initial-scale=1'>
+		
+	<!-- jQuery library -->
+	<script src='https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js'></script>
+	
+	<!-- Fonts from Google -->
+	<link href='https://fonts.googleapis.com/css?family=David+Libre|Inconsolata|Work+Sans' rel='stylesheet'>
+	
+	<!-- Css Crush Stylesheet -->
+	" . csscrush_tag('resources/styles/styles.css') . "
+	
+	<script>
+		//page load optimization
+	</script>
+		
+	<script>
+		//analytics
+	</script>
+</head>"
+?>
