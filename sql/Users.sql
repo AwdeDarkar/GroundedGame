f0f2751bb59e3f1322f30a93ce376b5f1c3f7d0e
Nathan Martindale <nmblenderdude0@gmail.com>
2018-01-20 20:57:24 -0600

    Updated default user password

--- sql/Users.sql
+++ sql/Users.sql
@@ -1,7 +1,7 @@
 
 INSERT INTO `Users` (`ID`, `Name`, `Hash`, `Verification`, `Email`, `DateJoined`, `Level`, `NameSafe`) VALUES
 (1, 'WildfireXIII', 'c1fa0745402bdb856a6cdf5916b23fcb19a0e6b244d710c7a32aa2f3f36ff05578d8c95f736267f8bb89349f023d2644f0919682465d1075aff0ff327f8f8a23', 'c0e73a79510d01835877b484ddbb87ee42b34951051310d81438f449cec9713aaf843456b9a7414085adc905c29c1e926adf4fc630c7a4a520a66e6e20aac728', 'nmblenderdude0@gmail.com', '2017-12-27', 2, 'wildfirexiii'),
-(2, 'Darkar', '3c7c761ecab42fe9e595451a7007ed52830cb567eb167878a85630129419cf7acd11ecf42c729c316b3424e20df76d7f646c09848b553f35477d67552675ee68', '412faeede740661fbb6e36bbb1de5980822d554ce19cd3061268ee1cb89f6c97bf3c901f7ac2841c2ddb7c0caf3a66dde073b9b9193943db6631eed6798afa4e', 'darkardengeno@gmail.com', '2017-12-27', 2, 'darkar');
+(2, 'Darkar', 'c1fa0745402bdb856a6cdf5916b23fcb19a0e6b244d710c7a32aa2f3f36ff05578d8c95f736267f8bb89349f023d2644f0919682465d1075aff0ff327f8f8a23', '412faeede740661fbb6e36bbb1de5980822d554ce19cd3061268ee1cb89f6c97bf3c901f7ac2841c2ddb7c0caf3a66dde073b9b9193943db6631eed6798afa4e', 'darkardengeno@gmail.com', '2017-12-27', 2, 'darkar');
 
 ALTER TABLE `Users`
   MODIFY `ID` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
