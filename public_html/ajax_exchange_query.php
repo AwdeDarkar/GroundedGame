b429392d848fb2a6e93406ae444eaa34cb9a7230
Nathan Martindale <nmblenderdude0@gmail.com>
2018-02-10 21:42:18 -0600

    Added ajax page for exchange table

--- public_html/ajax_exchange_query.php
+++ public_html/ajax_exchange_query.php
@@ -11,50 +11,96 @@ if ($_GET['w'])
 elseif($_SESSION['world']) { $world = $_SESSION['world']; }
 
 
-// query all exchanges by default
-/*if ($stmt = $mysqli->prepare("SELECT Orders.ID, Orders.SellingFactionID, Orders.RCID, Orders.AmountRemaining, .ID FROM Users, Factions, Worlds WHERE Factions.WorldID = Worlds.ID AND Factions.UserID = Users.ID AND Worlds.NameSafe = ? AND Users.ID = ?"))
+# get world id
+
+if ($stmt = $mysqli->prepare("SELECT Worlds.ID FROM Worlds WHERE Worlds.NameSafe = ?"))
 {
-	$stmt->bind_param('ss', $world, $userid);
+	$stmt->bind_param('ss', $world);
 	$stmt->execute();
 	$stmt->store_result();
-	$stmt->bind_result($facID);
+	$stmt->bind_result($worldID);
 	$stmt->fetch();
 }
 else { throw_msg(300, $httpReferer, "create_faction.php", 39); }
 
 
 
-echo("
-<table>
-	<tr>
-		<th>Bunker X</th>
-		<th>Bunker Y</th>
-		<th>Bunker Owner</th>
-		<th></th>
-	</tr>
-");
-
-if ($stmt = $mysqli->prepare("SELECT Bunkers.ID, Bunkers.WorldX, Bunkers.WorldY, Factions.Name, Factions.NameSafe, Factions.ID FROM ((Bunkers LEFT OUTER JOIN Factions ON Bunkers.FactionID = Factions.ID) INNER JOIN Worlds on Bunkers.WorldID = Worlds.ID) WHERE Worlds.NameSafe = ?"))
+$o_ids = array();
+$o_rNames = array();
+$o_fNames = array();
+$o_amts = array();
+$o_costs = array();
+$o_costpers = array();
+$o_dates = array();
+$o_statuses = array();
+$o_comments = array();
+// query all exchanges by default
+if ($stmt = $mysqli->prepare("
+	SELECT 
+		Orders.ID, 
+		Resources.Name, 
+		Factions.Name, 
+		Orders.AmountRemaining, 
+		Orders.Cost, 
+		Orders.Status, 
+		Orders.DatePosted, 
+		Orders.Comment
+	FROM Orders, Factions, Resources 
+	WHERE
+		Resources.ID = Orders.RID AND
+		Factions.ID = Orders.SellingFactionID AND
+		Orders.WID = ?"))
 {
-	$stmt->bind_param('s', $world);
+	$stmt->bind_param('ss', $worldID);
 	$stmt->execute();
 	$stmt->store_result();
-	$stmt->bind_result($BunkerID, $x, $y, $Name, $NameSafe, $BunkerFacID);
-	
-	while($stmt->fetch())
+	$stmt->bind_result($oid, $rname, $fname, $oamt, $ocost, $ostatus, $odate, $ocomment);
+	while ($stmt->fetch())
 	{
-		echo("<tr>");
-		echo("<td>$x</td><td>$y</td>");
-		if (is_null($Name)) { echo("<td>Unowned</td>"); }
-		else { echo("<td>$Name</td>"); }
-
-		if ($BunkerFacID == $facID) { echo("<td><a href='bunker.php?w=$world&b=$BunkerID'>Manage</a></td>"); }
-		else { echo("<td></td>"); }
-		
-		echo("</tr>");
+		array_push($o_ids, $oid);
+		array_push($o_rNames, $rname);
+		array_push($o_fNames, $fname);
+		array_push($o_amts, $oamt);
+		array_push($o_costs, $ocost);
+		$costper = (float)$ocost / (float)$oamt;
+		array_push($o_costpers, $costper);
+		array_push($o_dates, $odate);
+		array_push($o_statuses, $ostatus);
+		array_push($o_comments, $ocomment);
 	}
-
-	echo("</table>");
 }
+else { throw_msg(300, $httpReferer, "create_faction.php", 39); }
+
+
 ?>
- */
+
+<table>
+	<tr>
+		<th>Resource</th>
+		<th>Cost</th>
+		<th>Amount</th>
+		<th>(Total)</th>
+		<th>Seller</th>
+		<th>Comment</th>
+		<th>Posted</th>
+	</tr>
+
+<?php
+
+for ($i = 0; $i < count($o_ids); $i++)
+{
+	echo("<tr>
+			<td>".$o_rNames[$i]."</td>
+			<td>$".$o_costpers[$i]."</td>
+			<td>".$o_amts[$i]."</td>
+			<td>($".$o_costs[$i].")</td>
+			<td>".$o_fNames[$i]."</td>
+			<td>".$o_comments[$i]."</td>
+			<td>".$o_dates[$i]."</td>
+		</tr>");
+}
+	
+?>
+
+</table>
+ 
--- public_html/exchange.php
+++ public_html/exchange.php
@@ -44,6 +44,25 @@ else { throw_msg(300, $httpReferer, "create_faction.php", 39); }
 
 <a href='exchange_resource_sell.php'>Post Resource Sell</a>
 
+<h2>Current Sells</h2>
+<div id='sells'>
+</div>
+
+<script type='text/javascript'>
+function getFactionList()
+{
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			document.getElementById("sells").innerHTML = this.responseText;
+		}
+	};
+	xhttp.open("GET", "ajax_exchange_query.php?w=<?php echo("$world")?>", true);
+	xhttp.send();	
+
+	//setTimeout(getFactionList, 1000);
+}
+</script>
 
 
 </div>
