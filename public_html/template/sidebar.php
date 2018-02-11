8577307674182a16d0171df4ad659dd8635f67b2
Nathan Martindale <nmblenderdude0@gmail.com>
2018-02-10 19:53:59 -0600

    Added exchange page to sidebar

--- public_html/template/sidebar.php
+++ public_html/template/sidebar.php
@@ -3,6 +3,7 @@ echo "
 <div class='sidebar'>
 <a href='worlds.php'>World List</a><br>
 <a href='world.php'>World</a><br>
+<a href='exchange.php'>Exchange</a><br>
 <a href='admin.php'>Admin</a><br>
 <a href='bunker.php'>Bunker</a><br>
 <a href='index.php'>Index</a><br>
