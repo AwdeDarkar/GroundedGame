98739af98146ad03fb9eedbfedcf7b43d9addd7a
AwdeDarkar <darkardengeno@gmail.com>
2018-02-24 23:16:03 -0500

    It helps to define variables _before_ you use them.

--- public_html/create_message_group.php
+++ public_html/create_message_group.php
@@ -41,6 +41,7 @@ $facName = getFactionName($facID);
 			{
 				selected_indicies.splice(inselect, 1);
 				selected_names.splice(inselect, 1);
+				var member_element = document.getElementById("M" + ind);
 				member_element.style.innerHTML = member_names[i];
 				member_element.title = "Add this member to your group";
 				var selected_members_listing = "";
