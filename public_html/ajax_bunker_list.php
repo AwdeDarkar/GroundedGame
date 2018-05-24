341b7dcf86ec184f64be9e5129286bcf94bb4627
AwdeDarkar <darkardengeno@gmail.com>
2018-05-23 22:43:22 -0400

    Started fancy world map

--- public_html/ajax_bunker_list.php
+++ public_html/ajax_bunker_list.php
@@ -5,6 +5,8 @@ include("../includes/common.php");
 $world = getCurrentWorld();
 $facID = getFactionID(LOGGED_USER_ID, $world);
 
+$listString = "[ ";
+
 echo("
 <table>
 	<tr>
@@ -27,8 +29,17 @@ if ($stmt = $mysqli->prepare("SELECT Bunkers.ID, Bunkers.WorldX, Bunkers.WorldY,
 	{
 		echo("<tr>");
 		echo("<td>$x</td><td>$y</td>");
-		if (is_null($Name)) { echo("<td>Unowned</td>"); }
-		else { echo("<td>$Name</td>"); }
+		$listString .= ", [$x, $y, ";
+		if (is_null($Name))
+		{
+			$listString .= "\"Unowned\"]";
+			echo("<td>Unowned</td>");
+		}
+		else 
+		{
+			echo("<td>$Name</td>");
+			$listString .= "\"" . $Name . "\"]";
+		}
 
 		if ($BunkerFacID == $facID) { echo("<td><a href='bunker.php?w=$world&b=$BunkerID'>Manage</a></td>"); }
 		else { echo("<td></td>"); }
@@ -36,6 +47,6 @@ if ($stmt = $mysqli->prepare("SELECT Bunkers.ID, Bunkers.WorldX, Bunkers.WorldY,
 		echo("</tr>");
 	}
 
-	echo("</table>");
+	echo("</table>???$listString ]");
 }
 ?>
