9ac81f2a704f1e68854b46ca2e87023cb5138f1b
Nathan Martindale <nmblenderdude0@gmail.com>
2018-05-23 21:03:32 -0500

    Added equipment handling to start_production

--- public_html/start_production.php
+++ public_html/start_production.php
@@ -69,6 +69,10 @@ if ($stmt = $mysqli->prepare("
 else { throw_msg(300, $httpReferer, "create_faction.php", 39); }
 
 
+
+
+
+
 $preparedStatementIDs = array();
 $typesString = "";
 for ($i = 0; $i < count($processIDs); $i++) { $typesString .= "s"; }
@@ -79,7 +83,11 @@ $preparedStatementIDs[] = &$typesString;
 for ($i = 0; $i < count($processIDs); $i++) { $preparedStatementIDs[] = &$processIDs[$i]; }
 
 
-$query = "
+
+
+
+# get all associated process component information for each possible process
+$query_pc = "
 	SELECT 
 		Processes.ID,
 		ProcessComponents.ID,
@@ -96,7 +104,19 @@ $questionString = "";
 for ($i = 0; $i < count($processIDs); $i++) { $questionString .= "?,"; }
 $questionString = rtrim($questionString,',');
 
-$query .= $questionString.")";
+$query_pc .= $questionString.")";
+
+# get all resource collection associated equipment
+$query_eq = "
+	SELECT
+		Equipment.ID,
+		Equipment.RCID
+	FROM Equipment, ResourceCollections
+	WHERE
+		ResourceCollections.ID = Equipment.RCID AND
+		Equipment.RCID in (".$questionString.")";
+
+
 
 
 $pcProcessIDs = array();
@@ -105,7 +125,7 @@ $pcNames = array();
 $pcTypes = array();
 $pcReq = array(); # required amount of the stuff
 # get list of all process components user can do
-if ($stmt = $mysqli->prepare($query))
+if ($stmt = $mysqli->prepare($query_pc))
 {
 	#$stmt->bind_param('s', $bunkerID);
 
@@ -131,6 +151,36 @@ $uniqueProcessIDs = array_unique($processIDs);
 $uniqueProcessNames = array_unique($processNames);
 
 
+$eqIDs = array();
+$eqRCIDs = array();
+if ($stmt = $mysqli->prepare($query_eq))
+{
+	#call_user_func_array(array($stmt, 'bind_param'), $processIDs);
+	call_user_func_array(array($stmt, 'bind_param'), $preparedStatementIDs);
+	
+	$stmt->execute();
+	$stmt->store_result();
+	$stmt->bind_result($id,$rcid);
+	while ($stmt->fetch())
+	{
+		array_push($eqIDs, $id);
+		array_push($eqRCIDs, $rcid);
+	}
+}
+else { throw_msg(300, $httpReferer, "create_faction.php", 39); }
+
+# organize equipment into dictionary by RCID
+$equipment = array();
+for($i = 0; $i < count($eqIDs); $i++)
+{
+	if (!array_key_exists($eqRCIDs[$i], $equipment)) { $equipment[$eqRCIDs[$i]] = array(); }
+	array_push($equipment[$eqRCIDs[$i]], $eqIDs[$i]);
+}
+
+# get list of all 
+
+
+
 displayStart();
 ?>
 
@@ -138,33 +188,53 @@ displayStart();
 
 <script type="text/javascript">
 
-function useResource(pid, rid, amt, pcid)
+function useResource(pid, rid, amt, pcid, eid)
 {
-	var elem = document.getElementById("p" + pid + "_pc" + pcid);
-	var btn = document.getElementById("btn" + pid + "_" + rid);
+	var hiddenElemPc = document.getElementById("p" + pid + "_pc" + pcid);
+	var hiddenElemEq = document.getElementById("p" + pid + "_pc" + pcid + "_eq");
+	var btn = document.getElementById("btn" + pid + "_" + rid + "_" + eid);
 
 	// check if this rid is already there (if so, toggle it)
 	subtract = false;
-	var rids = elem.value.split(','); 
+	var rids = hiddenElemPc.value.split(','); 
+	var eids = hiddenElemEq.value.split(',');
 	console.log(rids);
-	if (rids.indexOf(rid.toString()) != -1)
+	console.log(eids);
+
+	if (eid == 0)
 	{
-		console.log("Already there!");
-		subtract = true;
-		var index = rids.indexOf(rid.toString());
-		rids.splice(index, 1);
+		if (rids.indexOf(rid.toString()) != -1)
+		{
+			console.log("Already there!");
+			subtract = true;
+			var index = rids.indexOf(rid.toString());
+			rids.splice(index, 1);
+		}
+	}
+	else
+	{
+		if (eids.indexOf(eid.toString()) != -1)
+		{
+			console.log("Already there!");
+			subtract = true;
+			var index = eids.indexOf(eid.toString());
+			eids.splice(index, 1);
+		}
 	}
 
 	if (subtract) 
 	{ 
 		btn.style = '';
-		elem.value = rids.join(','); 
+		if (eid == 0) { hiddenElemPc.value = rids.join(','); }
+		else { hiddenElemEq.value = eids.join(','); }
 	}
 	else
 	{
 		btn.style = 'color: red;';
-		if (elem.value != "") { elem.value += ","; }
-		elem.value += rid;
+		if (hiddenElemPc.value != "" && eid == 0) { hiddenElemPc.value += ","; }
+		else if (hiddenElemEq.value != "" && eid != 0) { hiddenElemEq.value += ","; }
+		if (eid == 0) { hiddenElemPc.value += rid; }
+		else { hiddenElemEq.value += eid; }
 	}
 
 	var display = document.getElementById("disp" + pid + "_" + pcid);
@@ -218,7 +288,9 @@ for ($i = 0; $i < count($uniqueProcessNames); $i++)
 		if ($pcProcessIDs[$j] == $uniqueProcessIDs[$i] && $pcTypes[$j] != 1)
 		{
 			echo("<tr><input type='hidden' id='p".$uniqueProcessIDs[$i]."_pc".$pcIDs[$j]."' name='p".$uniqueProcessIDs[$i]."_pc".$pcIDs[$j]."' value=''><td/><td>".$pcNames[$j]."</td><td>".$pcReq[$j]."</td><td>");
-			
+			echo("<tr><input type='hidden' id='p".$uniqueProcessIDs[$i]."_pc".$pcIDs[$j]."_eq' name='p".$uniqueProcessIDs[$i]."_eq".$pcIDs[$j]."_eq' value=''><td/><td>".$pcNames[$j]."</td><td>".$pcReq[$j]."</td><td>");
+
+				
 			$foundRes = false;
 			$ownedString = "";
 			# check for resource collections of this requirement
@@ -227,8 +299,20 @@ for ($i = 0; $i < count($uniqueProcessNames); $i++)
 				if ($processComponentIDs[$k] == $pcIDs[$j]) 
 				{ 
 					$foundRes = true;
-					#$ownedString .= $processAmts[$k].","; 
-					$ownedString .= "<button id='btn".$pcProcessIDs[$j]."_".$processResourceIDs[$k]."' type='button' onclick='useResource(".$pcProcessIDs[$j].",".$processResourceIDs[$k].",".$processAmts[$k].",".$pcIDs[$j].");'>".$processAmts[$k]."</button>";
+					
+					# equipment component
+					if ($pcTypes[$j] == 2)
+					{
+						$equipments = $equipment[processResourceIDs[$k]];
+						for($m = 0; $m < count($equipments); $m++)
+						{
+							$ownedString .= "<button id='btn".$pcProcessIDs[$j]."_".$processResourceIDs[$k]."_".$equipments[$m]."' type='button' onclick='useResource(".$pcProcessIDs[$j].",".$processResourceIDs[$k].",1,".$pcIDs[$j].",".$equipments[$m].");'>1</button>";
+						}
+					}
+					else
+					{
+						$ownedString .= "<button id='btn".$pcProcessIDs[$j]."_".$processResourceIDs[$k]."_0' type='button' onclick='useResource(".$pcProcessIDs[$j].",".$processResourceIDs[$k].",".$processAmts[$k].",".$pcIDs[$j].",0);'>".$processAmts[$k]."</button>";
+					}
 				}
 			}
 			# remove trailing comma
