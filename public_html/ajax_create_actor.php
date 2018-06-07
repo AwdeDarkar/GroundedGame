62e09032b453298b627012ea014d870f3453e86e
AwdeDarkar <darkardengeno@gmail.com>
2018-06-06 21:57:52 -0400

    More debugs

--- public_html/ajax_create_actor.php
+++ public_html/ajax_create_actor.php
@@ -14,7 +14,7 @@ if ($stmt = $mysqli->prepare("SELECT ID FROM Resources WHERE Name='People'"))
 	$stmt->bind_result($RID);
 	$stmt->fetch();
 }
-else { throw_msg(300, $httpReferer, "bunker.php", 17); }
+else { throw_msg(17, $httpReferer, "bunker.php", 17); }
 
 if ($stmt = $mysqli->prepare("SELECT ID FROM ResourceCollections WHERE BunkerID=? and ResourceID=?"))
 {
@@ -24,7 +24,7 @@ if ($stmt = $mysqli->prepare("SELECT ID FROM ResourceCollections WHERE BunkerID=
 	$stmt->bind_result($RCID);
 	$stmt->fetch();
 }
-else { throw_msg(300, $httpReferer, "bunker.php", 27); }
+else { throw_msg(27, $httpReferer, "bunker.php", 27); }
 
 if ($stmt = $mysqli->prepare("SELECT firstname FROM Names ORDER BY RAND() LIMIT 1"))
 {
@@ -33,20 +33,20 @@ if ($stmt = $mysqli->prepare("SELECT firstname FROM Names ORDER BY RAND() LIMIT 
 	$stmt->bind_result($Name);
 	$stmt->fetch();
 }
-else { throw_msg(300, $httpReferer, "bunker.php", 36); }
+else { throw_msg(36, $httpReferer, "bunker.php", 36); }
 
 if ($stmt = $mysqli->prepare("INSERT INTO Actors(Name, ResourceID, RCID, Hitpoints, JobID) VALUES (?, ?, 10, 0)"))
 {
 	$stmt->bind_param("sss", $Name, $RID, $RCID);
 	$result = $stmt->execute();
 }
-else { throw_msg(300, $httpReferer, "bunker.php", 43); }
+else { throw_msg(43, $httpReferer, "bunker.php", 43); }
 
 if ($stmt = $mysqli->prepare("UPDATE ResourceCollections SET Amount = Amount + 1 WHERE ID=?"))
 {
 	$stmt->bind_param("s", $RCID);
 	$result = $stmt->execute();
 }
-else { throw_msg(300, $httpReferer, "bunker.php", 50); }
+else { throw_msg(50, $httpReferer, "bunker.php", 50); }
 
 ?>
