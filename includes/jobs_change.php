d8908298e0eed7c83e04ecaff5fe3831591c8cad
Nathan Martindale <nmblenderdude0@gmail.com>
2018-06-13 19:32:13 -0500

    Added jobskill handling to job builder

--- includes/jobs_change.php
+++ includes/jobs_change.php
@@ -5,7 +5,10 @@ if ($_POST['update_job_button'])
 	$id = tools_sanitize_data($_POST['update_job_button']);
 	$name = tools_sanitize_data($_POST["name_$id"]);
 	$description = tools_sanitize_data($_POST["description_$id"]);
+	$skills = tools_sanitize_data($_POST["skills_$id"]);
+	$skills_array = explode(',', $skills);
 	
+	// update jobs table
 	if ($stmt = $mysqli->prepare("UPDATE Jobs 
 		SET
 			Name = ?,
@@ -16,6 +19,42 @@ if ($_POST['update_job_button'])
 		$stmt->execute();
 	}
 	else { throw_msg(300, "admin_job_builder.php"); }
+
+	// drop old elems from jobskills table
+	if ($stmt = $mysqli->prepare("DELETE FROM JobSkills 
+		WHERE JID = ?"))
+	{
+		$stmt->bind_param('s', $id);
+		$stmt->execute();
+	}
+	else { throw_msg(300, "admin_job_builder.php"); }
+
+	// insert new elems from jobskills table
+	for($i = 0; $i < count($skills_array); $i++)
+	{
+		//$skills_array[$i];
+		
+		// obtain skill id
+		$sid = 0;
+		if ($stmt = $mysqli->prepare("SELECT Skills.ID FROM Skills, WHERE Skills.Name = ?"))
+		{
+			$stmt->bind_param('s', $skills_array[$i]);
+			$stmt->execute();
+			$stmt->store_result();
+			$stmt->bind_result($sid);
+			$stmt->fetch()
+		}
+		else { throw_msg(300, $httpReferer, "create_faction.php", 39); }
+		
+			
+		// insert new job skill
+		if ($stmt = $mysqli->prepare("INSERT INTO JobSkils (JID, SID) VALUES (?,?)"))
+		{
+			$stmt->bind_param('ss', $id, $sid);
+			$stmt->execute();
+		}
+		else { throw_msg(300, "admin_job_builder.php"); }
+	}
 	throw_msg(100, 'admin_job_builder.php');
 }
 elseif ($_POST['new_job_button'])
