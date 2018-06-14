6a15e002538741ed017c483262d6219a3d164157
AwdeDarkar <darkardengeno@gmail.com>
2018-06-13 21:31:37 -0400

    Created changeJobs.php

---
+++ public_html/changeJob.php
@@ -0,0 +1,14 @@
+<?php
+define(PERMISSION_LEVEL, 1);
+include("../includes/common.php");
+
+$actorID = tools_sanitize_data($_GET['a']);
+$jobID = tools_sanitize_data($_GET['j']);
+
+if ($stmt = $mysqli->prepare("UPDATE Actors SET JID = ?  WHERE ID = ?"))
+{
+	$stmt->bind_param("ss", $jobID, actorID);
+	$result = $stmt->execute();
+}
+
+?>
